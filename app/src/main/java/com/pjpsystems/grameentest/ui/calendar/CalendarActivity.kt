eccc3e6179c43bdce8dac06716f95a1f91f39c1f
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-05-16 23:50:13 +0800

    commiting changes to CalendarActivity

--- app/src/main/java/com/pjpsystems/grameentest/ui/calendar/CalendarActivity.kt
+++ app/src/main/java/com/pjpsystems/grameentest/ui/calendar/CalendarActivity.kt
@@ -2,8 +2,10 @@ package com.pjpsystems.grameentest.ui.calendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.apache.commons.lang3.time.DateUtils
 import com.pjpsystems.grameentest.R
 import com.pjpsystems.grameentest.databinding.ActivityCalendarBinding
+import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
 
@@ -12,5 +14,29 @@ class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = getString(R.string.select_date)
+        viewBinding = ActivityCalendarBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        configureCalendarView()
+        viewBinding.selectButton.setOnClickListener {
+
+        }
+        viewBinding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            // set the calendar date as calendar view selected date
+            val calendar = Calendar.getInstance()
+            calendar.set(year,month,dayOfMonth)
+
+            // set this date as calendar view selected date
+            view.date = calendar.timeInMillis
+        }
+    }
+
+    private fun configureCalendarView() {
+        val calendarView = viewBinding.calendarView
+        val currentDate = Date()
+        //set minimum date
+        calendarView.minDate = currentDate.time
+        //set maximum date (2 months ahead)
+        val maxDate: Date = DateUtils.addMonths(currentDate, 2)
+        calendarView.maxDate = maxDate.time
     }
 }
