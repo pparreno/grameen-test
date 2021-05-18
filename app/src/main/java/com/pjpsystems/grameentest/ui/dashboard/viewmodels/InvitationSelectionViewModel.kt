5ab7c7756234eb3c43809eda81995505f333d971
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-05-18 14:08:51 +0800

    commiting recent stable changes

---
+++ app/src/main/java/com/pjpsystems/grameentest/ui/dashboard/viewmodels/InvitationSelectionViewModel.kt
@@ -0,0 +1,7 @@
+package com.pjpsystems.grameentest.ui.dashboard.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class InvitationSelectionViewModel(application: Application) : AndroidViewModel(application) {
+}
